6253e5b6b641dc25d379ce9da7cb64e3bafb08b3
唐睿骞 <ruiqian.tang@chaihuo.org>
2021-04-22 17:48:54 +0800

    更新 lcd1602.py

--- example/lcd1602.py
+++ example/lcd1602.py
@@ -3,8 +3,8 @@ from lcd1602 import LCD1602
 from machine import I2C,Pin
 from time import sleep
 i2c = I2C(1,scl=Pin(7), sda=Pin(6), freq=400000)
-d = LCD1602.display(i2c, 2, 16)
-#d = LCD1602_RGB.display(i2c, 2, 16)
+d = LCD1602(i2c, 2, 16)
+#d = LCD1602_RGB(i2c, 2, 16)
 #d.set_rgb(255, 0, 0)
 sleep(1)
 d.home()
