7f7cb3ecaddefc98f16b0b1ea74f922f47e243ba
唐睿骞 <ruiqian.tang@chaihuo.org>
2021-05-28 11:05:35 +0800

    更新 lcd1602.py

--- example/lcd1602.py
+++ example/lcd1602.py
@@ -3,7 +3,9 @@ from lcd1602 import LCD1602
 from machine import I2C,Pin
 from time import sleep
 i2c = I2C(1,scl=Pin(7), sda=Pin(6), freq=400000)
+# Grove - 16x2 LCD
 d = LCD1602(i2c, 2, 16)
+# Grove - LCD RGB Backlight
 #d = LCD1602_RGB(i2c, 2, 16)
 #d.set_rgb(255, 0, 0)
 sleep(1)
